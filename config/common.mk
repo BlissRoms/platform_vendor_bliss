baf6791ccdb41ce1fad32688fb603d5d42de8330
blueyes27 <brian.w.petroski@gmail.com>
2015-12-23 21:48:05 -0800

    Adding OmniSwitch to build
    Change-Id: I181a9f1f5e7bf367d7283df92eca4043bc1caaa2

--- config/common.mk
+++ config/common.mk
@@ -161,8 +161,8 @@ PRODUCT_PACKAGES += \
 # Other packages
 PRODUCT_PACKAGES += \
     BlissOTA \
-    BlissSettings
-
+    BlissSettings \
+    OmniSwitch
 # Extra tools in Bliss
 PRODUCT_PACKAGES += \
     libsepol \

3cd3957a32c66df1e846fbb055d83dace02e2dc7
Adnan Begovic <adnan@cyngn.com>
2016-03-25 12:45:37 -0700

    cm: Introduce partner feature xml.
    Change-Id: Ic4ab2c85fb2935646438b9063d98ee0333c927f5
    TICKET: CYNGNOS-2293

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -6,7 +6,8 @@ PRODUCT_COPY_FILES += \
     vendor/bliss/config/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
     vendor/bliss/config/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
     vendor/bliss/config/permissions/org.cyanogenmod.telephony.xml:system/etc/permissions/org.cyanogenmod.telephony.xml \
-    vendor/bliss/config/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml
+    vendor/bliss/config/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
+    vendor/bliss/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml
 
 # CM Platform Library
 PRODUCT_PACKAGES += \

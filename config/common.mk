ResurrectionRemix/android_vendor_resurrection

vendor: add SuperSU 2.52
Signed-off-by: Akhil Narang <something@akhilnarang.xyz>

config/common.mk
@@ -227,6 +227,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 CM_DISPLAY_VERSION := $(CM_VERSION)
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 

BlissRoms/platform_vendor_bliss

Conflicts:
	config/common.mk
Change-Id: I6cb88aa24d45b7fff3969e94bde7f665ef4ede62

config/common.mk
@@ -256,6 +256,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ota.version=$(BLISS_OTA_VERSION) \
     ro.ota.device=$(TARGET_DEVICE) \
     ro.ota.manifest=$(BLISS_DEVICE_URL)/ota$(BLISS_OTA_XMLVER).xml
+    
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/bliss/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
60f6dd3bf342153b900dfefad0a5560e0925259b
BitO BSessiOn <bitobsession@gmail.com>
2016-09-11 14:05:33 +0200

    Add New Bliss Bootanimations
    Adopted classic structure. One zip for each resolution
    
    If you get stuck in boot animation, probably you have to set in your device.mk file:
    
    TARGET_BOOTANIMATION_HALF_RES := true
    
    Don't forget to add height and width params for proper boot animation selection.
    
    For exanple:
    
    TARGET_SCREEN_HEIGHT := 1920
    TARGET_SCREEN_WIDTH := 1200
    
    *Lower of both values is picked as bootanim (1200.zip in this example)
    
    Change-Id: I0de18afda9ab4d1d6ded7b83e930173558b35944
    Reviewed-on: http://review.blissroms.com/4213
    Reviewed-by: Dark Eyes <Dark_Eyes_@hotmail.com>
    Tested-by: Dark Eyes <Dark_Eyes_@hotmail.com>
    
    New Bliss N Boot Animations
    
    Squared PNG files for each resolution (lowest resolution value):
    
    480, 600, 720, 800, 1080, 1200, 1440
    
    Big Thx to Jon West <electrikjesus@gmail.com> for original render !
    
    Change-Id: Ia0656a3bbdc0076c87118136d67a67e5cb571488
    Reviewed-on: http://review.blissroms.com/4249
    Reviewed-by: BitO BSessiOn <bitobsession@gmail.com>
    Reviewed-by: Jackeagle <jackeagle102@gmail.com>
    Tested-by: Jackeagle <jackeagle102@gmail.com>
    
    Add 1600.zip resolution to Boot Animation
    
    Change-Id: I087d50b4a85acd93fc80442676d2c61f5f3e28c8
    Reviewed-on: http://review.blissroms.com/4283
    Reviewed-by: BitO BSessiOn <bitobsession@gmail.com>
    Reviewed-by: Dark Eyes <Dark_Eyes_@hotmail.com>
    Tested-by: Dark Eyes <Dark_Eyes_@hotmail.com>

--- config/common.mk
+++ config/common.mk
@@ -17,9 +17,43 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
-# Bootanimation
+ # Bootanimation support
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,, $(shell ls vendor/bliss/prebuilt/common/bootanimation))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
+PRODUCT_BOOTANIMATION := vendor/bliss/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip
+else
+PRODUCT_BOOTANIMATION := vendor/bliss/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+endif
+
 PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+    $(PRODUCT_BOOTANIMATION):system/media/bootanimation.zip
+endif
 
 # init.d support
 PRODUCT_COPY_FILES += \

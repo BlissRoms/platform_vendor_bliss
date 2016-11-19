8869195322b0a2a2a1fdbbaeb77b869ea39dc946
BitO BSessiOn <bitobsession@gmail.com>
2016-11-19 13:20:59 +0100

    Add telephony profile
    APN list, default ringtone and apps
    
    Change-Id: I191068be61f38e948ca48ca09b548cc24d7c4ea1

---
+++ config/telephony.mk
@@ -0,0 +1,13 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    messaging \
+    Stk \
+    CellBroadcastReceiver
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Orion.ogg

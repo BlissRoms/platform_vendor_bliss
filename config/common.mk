ca0034a3b164ef514b93ce6c90f5e84551bcdcb5
Eric Park <ideamaneric@gmail.com>
2017-05-14 13:52:43 -0400

    Revert "Revert "Remove SuperSU [1 of 2]""
    This reverts commit f06a4ef5ac6917eb729939c8448bc314ea50a36d.
    
    Change-Id: Ibb7a1a5c09dade097889fb3767efd82daf02b2ad

--- config/common.mk
+++ config/common.mk
@@ -91,11 +91,6 @@ PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
     vendor/bliss/prebuilt/bin/blacklist:system/addon.d/blacklist
 
-# SuperSU
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    $(LOCAL_PATH)/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
-
 # Bliss Custom Apps
 PRODUCT_PACKAGES += \
 ThemeInterfacer

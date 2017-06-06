namdeptrai2003/android_vendor_dnd

vendor: Switch to Magisk for root solution [2/2]
This will add the Magisk Manager into the ROM and move the zip to be
flashed during ROM installation. Solution is stable and actively
supported by its creator. Commit actually written by @nathanchance,
with authorship given to topjohnwu since he did everything that is
being added.

Start with Magisk v11.1 & Magisk Manager v4.1

Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

config/common.mk
@@ -248,6 +248,14 @@ PRODUCT_PACKAGES += \
     su
 endif
 
+# Magisk Manager
+PRODUCT_PACKAGES += \
+    MagiskManager
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION = 1.0

BlissRoms/platform_vendor_bliss

Change-Id: I1e39910c237f54829d0031b17a19dbce2987deb1
Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

config/common.mk
@@ -108,5 +108,9 @@ PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/lib64/libbypass.so:system/lib64/libbypass.so
 endif
 
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+
 $(call inherit-product-if-exists, vendor/bliss/prebuilt/common/app/Android.mk)
 $(call inherit-product-if-exists, vendor/bliss/prebuilt/common/privapp/Android.mk)
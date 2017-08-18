c4059fba6f03b352f87c2b72ddcb43f101293e1c
Adrian DC <radian.dc@gmail.com>
2017-08-18 16:14:47 +0200

    build: Remove breaking global flags
    Change-Id: I9bbc75ce4701dc70c17b1d80c40e8b98e961dd2a

--- config/common.mk
+++ config/common.mk
@@ -113,6 +113,15 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 endif
 
+# Custom off-mode charger
+ifneq ($(WITH_BLISS_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    bliss_charger_res_images \
+    font_log.png \
+    libhealthd.bliss
+endif
+
 # Squisher Location
 SQUISHER_SCRIPT := vendor/bliss/tools/squisher
 

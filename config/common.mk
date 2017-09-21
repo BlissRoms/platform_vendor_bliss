873e93ea92ed056671be0880dbfad01a454c91da
sam3000 <sam@mortimer.me.uk>
2017-09-21 06:23:25 +0200

    vendor/bliss: Refactor Bliss init rc file
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I24f5b2e7189bf34734981b95d284a5eeefb0c36e

--- config/common.mk
+++ config/common.mk
@@ -66,10 +66,9 @@ PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/bliss/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
-# Init file
-PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/etc/init.bliss.rc:root/init.bliss.rc \
-    vendor/bliss/prebuilt/common/etc/init.blissupdater.rc:system/etc/init/init.blissupdater.rc
+# Copy all Bliss-specific init rc files
+	$(foreach f,$(wildcard vendor/bliss/prebuilt/common/etc/init/*.rc),\
+		$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \

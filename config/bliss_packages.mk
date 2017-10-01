7746a15cc666a25ae9b968c82e42071c55698266
Jackeagle <jackeagle102@gmail.com>
2017-10-01 05:27:24 -0400

    packages: Add LiveWallpaper Picker
    Change-Id: I3b48a54cd22bfb0cea3c74879098ac55c34e7dcf
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -24,7 +24,8 @@ PRODUCT_PACKAGES += \
     sqlite3 \
     strace \
     Terminal \
-    WallpaperPicker
+    WallpaperPicker \
+    LiveWallpapersPicker
 
 # Telephony packages
 PRODUCT_PACKAGES += \

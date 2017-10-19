678d7865c4e2bdac5ed4e4ea61e8edda6a7fec9f
Jackeagle <jackeagle102@gmail.com>
2017-10-19 12:01:38 +0200

    vendor/bliss: Remove duplicate stuffs
    Change-Id: I151fef99f51a2313a2a82cc1061b3ac54c0203d0
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -41,13 +41,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librsjni
-
-# Custom off-mode charger
-ifneq ($(WITH_CM_CHARGER),false)
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    cm_charger_res_images \
-    font_log.png \
-    libhealthd.cm
-endif
-

1eb3302c128ec97b504ec661ecafc496dd67d525
Joe Maples <joe@frap129.org>
2018-01-17 18:44:46 -0500

    [2/2] soong: Add a variable for proprietary libs
    Change-Id: Ia4d012ce20a1997672fc6c965626d3b8f6581c8f
    Signed-off-by: Joe Maples <joe@frap129.org>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -17,6 +17,7 @@ bliss_soong:
 	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),';  \
 	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
 	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false),';  \
-	echo '    "Use_legacy_rescaling":  $(if $(strip $(TARGET_OMX_LEGACY_RESCALING)),true,false)';  \
+	echo '    "Use_legacy_rescaling":  $(if $(strip $(TARGET_OMX_LEGACY_RESCALING)),true,false),';  \
+	echo '    "TargetUsesProprietaryLibs":  $(if $(strip $(TARGET_USES_PROPRIETARY_LIBS)),true,false)';  \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)

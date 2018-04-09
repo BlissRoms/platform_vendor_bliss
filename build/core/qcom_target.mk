350a6c9ab9728fad89b756cd0ba81c9c6b72ac9a
kunal <kunalshah912@gmail.com>
2018-04-09 16:27:19 +0000

    fix qcom_target.mk for hals issue
    Change-Id: Ia72e82d53da11a1b78ac4be3d5c1a58e1852ad32
    Signed-off-by: kunal <kunalshah912@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -28,16 +28,21 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     B_FAMILY := msm8226 msm8610 msm8974
     B64_FAMILY := msm8992 msm8994
     BR_FAMILY := msm8909 msm8916
-    UM_FAMILY := msm8937 msm8953
+    UM_3_18_FAMILY := msm8937 msm8953 msm8996
+    UM_4_4_FAMILY := msm8998 sdm660
 
     BOARD_USES_ADRENO := true
 
-    TARGET_USES_QCOM_BSP := true
+    # UM platforms no longer need this set on O+
+    ifneq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY) $(UM_4_4_FAMILY)),true)
+        TARGET_USES_QCOM_BSP := true
+    endif
 
     # Tell HALs that we're compiling an AOSP build with an in-line kernel
     TARGET_COMPILE_WITH_MSM_KERNEL := true
 
     ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
+        TARGET_USES_QCOM_BSP_LEGACY := true
         # Enable legacy audio functions
         ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
             USE_CUSTOM_AUDIO_POLICY := 1
@@ -67,12 +72,17 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
         QCOM_HARDWARE_VARIANT := msm8916
     else
-    ifeq ($(call is-board-platform-in-list, $(UM_FAMILY)),true)
-        MSM_VIDC_TARGET_LIST := $(UM_FAMILY)
-        QCOM_HARDWARE_VARIANT := msm8937
+    ifeq ($(call is-board-platform-in-list, $(UM_3_18_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(UM_3_18_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8996
+    else
+    ifeq ($(call is-board-platform-in-list, $(UM_4_4_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(UM_4_4_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8998
     else
         MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
         QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+    endif
     endif
     endif
     endif
@@ -87,6 +97,7 @@ $(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
 $(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
 $(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
+$(call set-device-specific-path,POWER,power,hardware/qcom/power)
 
 $(call ril-set-path-variant,ril)
 $(call wlan-set-path-variant,wlan-caf)
@@ -109,3 +120,4 @@ $(call wlan-set-path-variant,wlan)
 $(call bt-vendor-set-path-variant,bt)
 
 endif
+

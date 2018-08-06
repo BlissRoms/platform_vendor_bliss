21d8af5d9ffbbea12f4bceb28af37c42c0c5eb96
Jackeagle <jackeagle102@gmail.com>
2018-08-06 11:49:19 -0600

    vendor/bliss : Move all sound files to prebuilts - Move Themes too
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- prebuilt/common/sounds/GoogleAudio.mk
+++ prebuilt/common/sounds/GoogleAudio.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/bliss/google
+LOCAL_PATH := vendor/bliss/prebuilt/common/sounds
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/alarms/A_real_hoot.ogg:system/media/audio/alarms/A_real_hoot.ogg \

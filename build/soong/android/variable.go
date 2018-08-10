95566a7b5f2dfb6f3e2e38775b1ae338363d8133
Simon Shields <simon@lineageos.org>
2018-08-10 14:40:27 +1000

    soong: rework soong_config for Pie
    Change-Id: I3c5001f02975e5487ba66a02217f1d24d2021272
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,81 +3,34 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
-
-	Mtk_hardware struct {
-		Cflags []string
-	}
-
-	Cant_reallocate_omx_buffers struct {
-		Cflags []string
-	}
-
-	Qcom_bsp_legacy struct {
-		Cflags []string
-	}
-
-	Qti_flac_decoder struct {
-		Cflags []string
-	}
-	Use_legacy_rescaling struct {
-		Cflags []string
-	}
-
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
-	}
-
-	Target_uses_qsml struct {
-		Cflags []string
-		Shared_libs []string
-		Header_libs []string
-		Required []string
-	}
-
-	Target_uses_eigen struct {
-		Shared_libs []string
-		Required []string
-	}
-
 	Target_shim_libs struct {
 		Cppflags []string
 	}
-	
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
+	Uses_nvidia_enhancements struct {
+		Cppflags []string
+	}
+	Uses_qcom_bsp_legacy struct {
+		Cppflags []string
+	}
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
-	}
-	
-	Uses_nvidia_enhancements struct {
-		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Use_legacy_rescaling    *bool `json:",omitempty"`
-	Target_uses_qsml      *bool `json:",omitempty"`
-	Target_uses_eigen       *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
+	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }

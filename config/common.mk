c06520814eab5dff8e997b8c2fcde7ca476e1141
Jon West <electrikjesus@gmail.com>
2018-08-13 13:53:52 -0400

    [2/2] Add 'GoogleSans' font family
    Change-Id: I1e74dd8dc25bb9bf5db82e6e46f204e9d7ef16b0
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- config/common.mk
+++ config/common.mk
@@ -105,6 +105,18 @@ endif
 
 # Squisher Location
 SQUISHER_SCRIPT := vendor/bliss/tools/squisher
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/dot/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/dot/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/dot/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/dot/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/dot/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/dot/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG ?= false
 
 # Bliss Versioning System
 -include vendor/bliss/config/versions.mk

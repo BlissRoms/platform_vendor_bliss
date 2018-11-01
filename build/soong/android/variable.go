7c9edcffc97aefd879ef7cf9c3f03d40a316f10b
Jackeagle <jackeagle102@gmail.com>
2018-11-01 04:27:13 +0000

    Revert "soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS"
    This reverts commit 5598b617f2fa1b4558b73e33117923adc6e14a98.
    
    Change-Id: I8a75e08a097e984bc9ec32ef0927554846178c42
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,6 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
-	Needs_text_relocations struct {
-		Cppflags []string
-	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
@@ -30,7 +27,6 @@ type Product_variables struct {
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`

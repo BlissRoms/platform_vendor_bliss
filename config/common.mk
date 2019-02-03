omnirom/android_vendor_omni

vendor: enable control privaapp permissions enforcing
most (all?) treble device will probably come with "enforce" set in vendor props anyway

also add gapps permission as some gapps packages come with not complete or outdated permissions
therefore rename it so they dont overwrite it

Change-Id: I8c0c9c3b83160d9bca89c5a5cde56d8e7faca927

config/common.mk
@@ -69,7 +69,8 @@ PRODUCT_COPY_FILES += \
 
 #permissions
 PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml
+    vendor/omni/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml \
+    vendor/omni/prebuilt/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
 # custom omni sounds
 PRODUCT_PROPERTY_OVERRIDES += \

BlissRoms/platform_vendor_bliss

Conflicts:
	config/common.mk

config/common.mk
@@ -84,7 +84,9 @@ PRODUCT_COPY_FILES += \
 
 #permissions
 PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/etc/permissions/privapp-permissions-bliss.xml:system/etc/permissions/privapp-permissions-bliss.xml
+    vendor/bliss/prebuilt/etc/permissions/privapp-permissions-bliss.xml:system/etc/permissions/privapp-permissions-bliss.xml \
+    vendor/bliss/prebuilt/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
+
 
 # whitelist packages for location providers not in system
 PRODUCT_PROPERTY_OVERRIDES += \
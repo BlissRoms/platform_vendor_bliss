omnirom/android_vendor_omni

vendor: whitelist privapp permissions
idea from: https://review.lineageos.org/#/c/209859/

Change-Id: Iaed7ec4960175bf837425424f96bd452c81fb298
docs: https://source.android.com/devices/tech/config/perms-whitelist

config/common.mk
@@ -54,6 +54,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+#permissions
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml
+
 # custom omni sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=omni_ringtone1.ogg \

BlissRoms/platform_vendor_bliss

Conflicts:
	config/common.mk

config/common.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+#permissions
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/etc/permissions/privapp-permissions-bliss.xml:system/etc/permissions/privapp-permissions-bliss.xml
+
 # whitelist packages for location providers not in system
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.services.whitelist.packagelist=com.google.android.gms
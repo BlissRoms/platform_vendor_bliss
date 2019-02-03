omnirom/android_vendor_omni

vendor sepolicy genesis v2
Change-Id: I1ccf1ffed326c1ae7d008a847b11b58227b7e7e3

sepolicy/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Omni
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/omni/sepolicy/common
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/omni/sepolicy/private

BlissRoms/platform_vendor_bliss

sepolicy/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Bliss
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/bliss/sepolicy/common
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/bliss/sepolicy/private
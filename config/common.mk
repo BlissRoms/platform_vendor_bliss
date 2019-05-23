5682496d870024f352b4b1b2cdb24eb35533ff67
Jackeagle <jackeagle102@gmail.com>
2019-05-23 07:41:12 +0200

    vendor/bliss: Update Banner and Cleanup
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I985dc56f06debab9afd5cc8459107bcdfe995688

--- config/common.mk
+++ config/common.mk
@@ -63,8 +63,7 @@ PRODUCT_COPY_FILES += \
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/bliss/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/bliss/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+    vendor/bliss/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # Copy all Bliss-specific init rc files
 	$(foreach f,$(wildcard vendor/bliss/prebuilt/common/etc/init/*.rc),\

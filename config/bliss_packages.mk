35df313984c8ee7282a3769839df85a78dd7e111
Jackeagle <jackeagle102@gmail.com>
2019-07-20 05:04:32 -0400

    bliss: Move paackages to separate makefile
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I650d6f5437ab0c75e1107c48340f91349f7b8089
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ config/bliss_packages.mk
@@ -0,0 +1,85 @@
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Email \
+    ExactCalculator \
+    Exchange2 \
+    LiveWallpapersPicker \
+    messaging \
+    PhotoTable
+
+# Bliss Packages
+PRODUCT_PACKAGES += \
+    LatinIME \
+    Recorder \
+    LineageParts \
+    LineageSettingsProvider \
+    LockClock \
+    Profiles \
+    WeatherProvider
+
+# Bootanimation
+PRODUCT_PACKAGES += \
+    bootanimation.zip
+
+# Dex preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    TrebuchetQuickStep
+
+# Extra tools in bliss
+PRODUCT_PACKAGES += \
+    7z \
+    awk \
+    bash \
+    bzip2 \
+    curl \
+    getcap \
+    htop \
+    lib7z \
+    libsepol \
+    nano \
+    pigz \
+    powertop \
+    setcap \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
+# These packages are excluded from user builds
+PRODUCT_PACKAGES_DEBUG += \
+    procmem
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk \
+    CellBroadcastReceiver
+

abb157c6c476cb052520dc48410e04391581c28e
Jackeagle <jackeagle102@gmail.com>
2019-07-27 12:18:14 +0200

    bliss: Kill unwanted policy :|
    Change-Id: I0a5fff4fac5b8601bd28cca78bb01785343dd004
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- sepolicy/sepolicy.mk
+++
@@ -1,9 +0,0 @@
-#
-# This policy configuration will be used by all products that
-# inherit from Bliss
-#
-
-BOARD_SEPOLICY_DIRS += \
-    vendor/bliss/sepolicy/common
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/bliss/sepolicy/private

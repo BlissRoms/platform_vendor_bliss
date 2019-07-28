64d1d353b02da9d3db89dc8dbb78c61d754f89ae
Jackeagle <jackeagle102@gmail.com>
2019-07-28 14:31:18 -0400

    bliss: soong: Cleanup
    Change-Id: Icabe63372a8ac51672a20dcbd9827f144295d680
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,11 +3,25 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
-	Device_support_wait_for_qsee struct {
-		Cflags []string
-	}
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
+	}
+	Should_skip_waiting_for_qsee struct {
+		Cflags []string
+	}
+	Supports_hw_fde struct {
+		Cflags []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Supports_hw_fde_perf struct {
+		Cflags []string
+	}
+	Supports_legacy_hw_fde struct {
+		Cflags []string
+	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
 	}
 	Target_shim_libs struct {
 		Cppflags []string
@@ -28,23 +42,6 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
-	Should_skip_waiting_for_qsee struct {
-		Cflags []string
-	}
-	Supports_hw_fde struct {
-		Cflags []string
-		Header_libs []string
-		Shared_libs []string
-	}
-	Supports_hw_fde_perf struct {
-		Cflags []string
-	}
-	Supports_legacy_hw_fde struct {
-		Cflags []string
-	}
-	Target_process_sdk_version_override struct {
-		Cppflags []string
-	}
 }
 
 type ProductVariables struct {
@@ -55,11 +52,11 @@ type ProductVariables struct {
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
 	Supports_legacy_hw_fde  *bool `json:",omitempty"`
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_metadata_as_fde_key  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
-	Target_process_sdk_version_override *string `json:",omitempty"`
 }

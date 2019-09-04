aa7c1b7215c1c47f1bd7cfef173856285a330616
Jackeagle <jackeagle102@gmail.com>
2019-09-04 19:27:16 +0200

    vendor/bliss: Android Q Bringup
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -1,37 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := Lawnchair
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/Lawnchair.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := SubstratumSignature
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/SubstratumSignature.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_BUILT_MODULE_STEM := package.apk
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Longshot
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := priv-app/Longshot.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
 ################################
 # Copies the APN list file into system/etc for the product as apns-conf.xml.
 # In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)

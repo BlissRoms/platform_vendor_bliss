7f4e6f4780986d4a394c36cb5be36b134d3530dd
Jackeagle <jackeagle102@gmail.com>
2019-10-13 05:35:48 -0400

    roomservice: Track device repos from q
    Change-Id: Id50d0e3f03aafef45bfda2589aa156667cf263e5
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -38,9 +38,9 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "p9.0"
+default_rev = "q"
 # set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/omnirom"
+# example fetch="https://github.com/BlissRoms-Devices"
 default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"

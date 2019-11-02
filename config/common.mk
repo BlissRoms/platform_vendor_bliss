c61824ca569b0d7e3b754a6e71186db3be95db58
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-03 00:06:45 +0100

    Build adb_root
    Change-Id: I646b9db5543b47f777b443f601e9c2c6fa7ef08b

--- config/common.mk
+++ config/common.mk
@@ -153,6 +153,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    adb_root
+endif
+
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/bliss/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/bliss/overlay/common

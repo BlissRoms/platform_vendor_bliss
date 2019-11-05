8e489c060091e03a6b6dcda52e7668b01b0673c2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-05 18:33:41 +0200

    vendor: Add NexusWallpaperPrebuilt
    Change-Id: If5c2a22c50136e39dcac30229fc7dae90d057f4a
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -120,6 +120,17 @@ LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := NexusWallpapersStubPrebuilt2019Static
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := SettingsIntelligenceGooglePrebuilt
 LOCAL_OVERRIDES_PACKAGES := SettingsIntelligence
 LOCAL_MODULE_TAGS := optional

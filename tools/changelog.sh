151132865018a230f8748308af37257591d9b83e
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-25 03:55:38 +0200

    [SQUASHED] tools: changelog: improvements
    tools: changelog: Only fetch necessary dates
    
     * to save time upon dirty building
     * include the last 7 days instead of 30 days
    
    note: re-generating days is possible.
    Just delete the dates you want regenerated from the top (!!) of the
    changelog file in $OUT/system/etc/Changelog.txt
    2nd line of the file must be the date to fetch from!!
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    
    vendor: tools: Make number of changlog days generated stand out with colors
    
    vendor: changelog: Avoid using repo to cycle through all repos
    
    Use the manifest file to find all repos instead - this avoids all sorts
    of funky python related errors happening on some environments
    
    changelog: Prettify output for readability
    
    Include author name & email, relative date.
    Each commit now takes 2 lines and has a new line after
    
    changelog: Also generate for repos in local manifests
    Change-Id: I8f754e61b76898d0fb87e69b1ae059db3a8b4b10

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,17 +1,63 @@
-#!/bin/sh
+#!/bin/bash
+# note: re-generating days is possible. Just delete the dates you want regenerated
+# from the top (!!) of the changelog file in $OUT/system/etc/Changelog.txt
+# 2nd line of the file must be the date to fetch from!!
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
 
 export Changelog=Changelog.txt
+export PassedDays=7 # change this to limit max changelog days
+export MANIFEST="${TOP}/.repo/manifests/bliss.xml"
 
-if [ -f $Changelog ];
+if [ -f "${OUT}/system/etc/${Changelog}" ] # if changelog already generated
 then
-	rm -f $Changelog
+	cp "${OUT}/system/etc/${Changelog}" $Changelog
+	LastDate=`sed '2!d' $Changelog` # get 2nd line of changelog file
+	LastDate="$(echo -e "${LastDate}" | tr -d '[:space:]')" # get rid of whitespaces
+	# extract month day and year from string:
+	Month=${LastDate:0:2}
+	Day=${LastDate:3:2}
+	Year=${LastDate:6:4}
+	LastDate=`date -d "${Year}${Month}${Day}" +%s`
+	TimeNow=`date +%s` # current time
+	DayDiff=$(( (TimeNow - LastDate) / 86400 )) # n/o days passed
+	if [[ $DayDiff < $PassedDays ]]; then # don't fetch more than max days
+		export PassedDays=$DayDiff
+		mv $Changelog "${Changelog}.bak" # save current changelog for later
+	fi
+	touch $Changelog
+else
+	touch $Changelog
 fi
 
-touch $Changelog
+if [[ $PassedDays == 0 ]]; then
+	echo "Already have today"
+	rm $Changelog
+	rm "${Changelog}.bak"
+	exit 0
+else
+	echo -e "${GREEN}Regenerating log of last ${YELLOW}${PassedDays}${GREEN} days${NC}"
+	if [ -f "${OUT}/system/etc/${Changelog}" ]; then
+		rm "${OUT}/system/etc/${Changelog}"
+		rm "${OUT}/${Changelog}"
+	fi
+fi
 
-echo "Generating changelog..."
+# Build a list of all repos
+PROJECTPATHS=$(grep "<project" "${MANIFEST}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
 
-for i in $(seq 30);
+# Add repos in local manifest for DT changelog
+for lManifest in $TOP/.repo/local_manifests/*; do
+	PROJECTPATHS+=$(grep "<project" "${lManifest}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
+done
+
+echo -e "${GREEN}Generating changelog...${NC}"
+
+# Save current dir
+pDIR=$(echo $PWD)
+
+for i in $(seq $PassedDays);
 do
 export After_Date=`date --date="$i days ago" +%m-%d-%Y`
 k=$(expr $i - 1)
@@ -19,16 +65,30 @@ k=$(expr $i - 1)
 
 	# Line with after --- until was too long for a small ListView
 	echo '====================' >> $Changelog;
-	echo  "     "$Until_Date    >> $Changelog;
+	echo "     "$Until_Date     >> $Changelog;
 	echo '====================' >> $Changelog;
 	echo >> $Changelog;
 
 	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
+	for PROJECTPATH in ${PROJECTPATHS}; do
+		cd "${TOP}/${PROJECTPATH}"
+		if ! [[ -z $(git log --after=$After_Date --until=$Until_Date) ]]; then # only echo if there is a change
+			echo "[${PROJECTPATH}]" >> $pDIR/$Changelog
+			git log --format="%s <%ar> [%h]%nby: %an (%ae)" --after=$After_Date --until=$Until_Date >> $pDIR/$Changelog
+			echo >> $pDIR/$Changelog
+		fi
+	done
+	cd $pDIR
 	echo >> $Changelog;
 done
 
 sed -i 's/project/   */g' $Changelog
+
+if [ -f "${Changelog}.bak" ]
+then
+	cat "${Changelog}.bak" >> $Changelog # restore old changelog to the end of current one
+	rm "${Changelog}.bak"
+fi
 
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/

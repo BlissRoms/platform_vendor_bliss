09567dbb82552fcc2006f4d369b31bf764adb34f
AgentFabulous <kshitijgm@gmail.com>
2020-01-19 23:20:45 -0700

    vendor: Switch to Lawnchair again
    Change-Id: I8153874478569b13d83df69393f4b7f1534bdb5c
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -63,7 +63,7 @@ PRODUCT_PACKAGES += \
     BlissUpdater \
     BlissPapers \
     SettingsIntelligenceGooglePrebuilt \
-    NexusLauncherRelease \
+    Lawnchair \
     NexusWallpapersStubPrebuilt2019Static \
     GalleryGoPrebuilt \
     PixelThemesStub2019 \
@@ -96,7 +96,13 @@ PRODUCT_COPY_FILES += \
 # Bliss System Permission
 PRODUCT_COPY_FILES += \
     vendor/bliss/config/permissions/privapp-permissions-bliss-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-bliss-system.xml
-    
+
 # Bliss Product Permission
 PRODUCT_COPY_FILES += \
     vendor/bliss/config/permissions/privapp-permissions-bliss-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-bliss.xml
+
+# Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/bliss/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+

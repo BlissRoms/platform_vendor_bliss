LineageOS/android_vendor_lineage

default-permissions: Grant default permissions to com.android.exchange
Change-Id: I9dfcecde0e662b9e481696b661e16ac7e131a75c

config/common.mk
@@ -87,6 +87,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-power-whitelist.xml
 
+# Pre-granted permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/lineage-default-permissions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default-permissions/lineage-default-permissions.xml
+
 # Include AOSP audio files
 include vendor/lineage/config/aosp_audio.mk
 

BlissRoms/platform_vendor_bliss

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
+# Pre-granted permissions
+PRODUCT_COPY_FILES += \
+    vendor/bliss/config/permissions/lineage-default-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/lineage-default-permissions.xml
+
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
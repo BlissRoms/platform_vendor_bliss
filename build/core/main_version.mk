630f8db6a7d2856bbcad06dad1d70808ee93ee6a
Jackeagle <jackeagle102@gmail.com>
2020-04-22 20:31:21 +0200

    vendor/bliss: Move Bliss versioning to build/core
    - props dont get populated in system/build.prop without having ADDITIONAL_BUILD_PROPERTIES.
    
    Ref commit: https://github.com/LineageOS/android_vendor_lineage/commit/3d92f57ca0b0dab8098352c776fa03e9205e4b9a
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I683679c4cd1ab49af69fc396e3ffcef248c2798a

---
+++ build/core/main_version.mk
@@ -0,0 +1,7 @@
+ADDITIONAL_BUILD_PROPERTIES += \
+  ro.bliss.version=$(BLISS_DISPLAY_VERSION) \
+  ro.bliss.build.status=$(BLISS_BUILDTYPE) \
+  ro.bliss.releasetype=$(BLISS_BUILDTYPE) \
+  ro.bliss.changelog.version=Changelog-$(BLISS_VERSION) \
+  ro.bliss.fingerprint=$(BLISS_FINGERPRINT) \
+  ro.bliss.static.version=$(BLISS_VERSION_STATIC)

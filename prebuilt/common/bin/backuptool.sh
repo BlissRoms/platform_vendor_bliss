7fd3b8b5a640894fe0f06e970a99e0df7d6af4cf
Jackeagle <jackeagle102@gmail.com>
2020-04-22 17:32:53 +0200

    backuptool: Add static version for backuptool to work
    - So far it was using Bliss version and hence during version bumps it breaks backup tool and always updating version is not cool. Hence just pass a static version to it so that it works
    as expected.
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: If358a71fe8ef45f3180ed1e5e639c5bd27114c54

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -4,9 +4,9 @@
 #
 
 export C=/tmp/backupdir
+export V=11.0
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=18.0
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
@@ -36,7 +36,7 @@ if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.bliss.version=$V.*" $S/build.prop; then
+if ! grep -q "^ro.bliss.static.version=$V.*" $S/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

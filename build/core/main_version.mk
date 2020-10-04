c48a76762b34a4b5499651e8ea3fb5af9460ea2a
Jackeagle <jackeagle102@gmail.com>
2020-10-04 06:00:55 -0400

    bliss: Add support for Gapps and Vanilla variant builds
    - Default will be gapps variant
    - Disable AOSP Apps on gapps variant
    - Add BlissUpdater support for gapps/vanilla variants
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I405c55b075ab3b367f464aa0e78d730514917e53

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,4 +3,5 @@ ADDITIONAL_BUILD_PROPERTIES += \
   ro.bliss.build.status=$(BLISS_BUILDTYPE) \
   ro.bliss.changelog.version=Changelog-$(BLISS_VERSION) \
   ro.bliss.fingerprint=$(BLISS_FINGERPRINT) \
-  ro.bliss.static.version=$(BLISS_VERSION_STATIC)
+  ro.bliss.static.version=$(BLISS_VERSION_STATIC) \
+  ro.bliss.build.variant=$(BLISS_BUILD_VARIANT)

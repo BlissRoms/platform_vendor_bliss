ca8ce23c0e4cb6d40380c8ef0a36cc65833beb6c
rohan <purohit.rohan@gmail.com>
2020-10-14 03:45:26 -0400

    vendor: Add prebuilt settingsintelligencegoogle apk
    * from redfin-user-11-RD1A.200810.020-6792687-release-keys
    
    Change-Id: I275eefbd0ff60663808c93fda9d5175fe43d34c3

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -27,7 +27,8 @@ PRODUCT_PACKAGES += \
     BlissUpdater \
     Seedvault \
     WallpaperPicker2 \
-    LiveWallpapersPicker
+    LiveWallpapersPicker \
+    SettingsIntelligenceGooglePrebuilt
 
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \

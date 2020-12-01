a84f54a06ad2f7133ad3e1665e41bca00f09e637
Bruno Martins <bgcngm@gmail.com>
2020-12-01 21:22:46 +0000

    Bliss: Add Android Go specific configs
    Change-Id: I29f203b3fa19bc955976ee6c3da4142a4ab325bf
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ config/common_full_go_phone.mk
@@ -0,0 +1,5 @@
+# Set Bliss specific identifier for Android Go enabled products
+PRODUCT_TYPE := go
+
+# Inherit full common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)

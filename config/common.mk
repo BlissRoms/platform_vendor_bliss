308d6fbd8845d52ef1b96caebc92bb51c22bbba5
ghostrider-reborn <adithya.r02@outlook.com>
2021-03-04 20:07:43 +0000

    config: Force enable iorap by default
     * aosp disabled it once again in qpr2, lets enable it back
       and keep it that way
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -15,6 +15,8 @@ endif
 
 # IORap app launch prefetching using Perfetto traces and madvise
 PRODUCT_PRODUCT_PROPERTIES += \
+    iorapd.perfetto.enable=true \
+    iorapd.readahead.enable=true \
     ro.iorapd.enable=true
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)

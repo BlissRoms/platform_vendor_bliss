0310a757fd9295d323f695b9baee0879ea964238
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-03-12 14:09:27 +0100

    sensitive_pn: Run XML lint against the schema
    Change-Id: I083bd5834f5dd69bdbb5955e9f0e1f8dba760fe5
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- config/telephony.mk
+++ config/telephony.mk
@@ -4,8 +4,8 @@ WITH_GMS_COMMS_SUITE := true
 endif
 
 # Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+PRODUCT_PACKAGES += \
+    sensitive_pn.xml
 
 # World APN list
 PRODUCT_PACKAGES += \

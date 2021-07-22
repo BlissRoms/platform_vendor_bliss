6d7bf6471535d084662a6143898ae46ea22d0e75
Bruno Martins <bgcngm@gmail.com>
2021-07-22 09:35:57 +0000

    bliss: Add Android Go specific configs
    Launcher3 is also available as a Go version, so modify the existing
    configs to allow it to be shipped in Android Go enabled devices.
    
    Change-Id: I1a6f9490ea62e6bd1aaaa15ba15d6ff4109a0b8d

---
+++ config/common_mini_go_phone.mk
@@ -0,0 +1,5 @@
+# Set Bliss specific identifier for Android Go enabled products
+PRODUCT_TYPE := go
+
+# Inherit mini common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_mini_phone.mk)

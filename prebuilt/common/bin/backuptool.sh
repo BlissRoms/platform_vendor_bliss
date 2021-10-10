e7ed277dc68f8e048c06da40ac638bb5e38ee60f
Jackeagle <jackeagle102@gmail.com>
2021-10-10 05:27:28 -0400

    bliss: BlissRoms v15.0
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I17f0c70416f786f48f6f23285a71e1f72809540b

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -4,7 +4,7 @@
 #
 
 export C=/tmp/backupdir
-export V=11.0
+export V=12.0
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
 
--- prebuilt/common/bin/backuptool_ab.sh
+++ prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=11.0
+export V=12.0
 
 export ADDOND_VERSION=3
 

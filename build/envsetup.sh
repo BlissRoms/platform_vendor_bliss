bf4189d096decc52e9727272cbd629da085fe082
Danny Lin <danny@kdrag0n.dev>
2021-11-12 11:25:02 +0000

    build: Override host metadata
    * Matches the Android 12 build sandbox: https://android.googlesource.com/platform/build/+/refs/tags/android-12.0.0_r2/Changes.md
    
    Change-Id: I032127e2724b34e649bcc73c347c8e67df1f45b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1090,3 +1090,7 @@ function blissify()
     fi
     return $?
 }
+
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=nobody
+export BUILD_HOSTNAME=android-build

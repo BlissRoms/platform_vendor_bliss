ec47df5f289c90c91b36f622f898cadc17ed447c
Amy Grace <amy@blisslabs.org>
2022-01-02 13:27:34 +0000

    roomservice: Update branch to arcadia
    Change-Id: Icc2fe17c6247efa557ef00aa2cf02e2a97a35df9

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -38,7 +38,7 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "s"
+default_rev = "arcadia"
 # set this to the remote that you use for projects from your team repos
 # example fetch="https://github.com/BlissRoms-Devices"
 default_team_rem = "github"

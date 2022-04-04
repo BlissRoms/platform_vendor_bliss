e04a851cdb64ec3c22dbd0dad30daf0ba1ca21c8
LuK1337 <priv.luk@gmail.com>
2022-04-04 21:31:18 +0200

    config: Disable remote keyguard animation until it's fixed
    Fixes slow post biometric auth screen wakeup.
    
    Change-Id: I8f4824220f14a6fe93e792d15bcaf12fdf2c0ddf

--- config/common.mk
+++ config/common.mk
@@ -79,6 +79,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.wm.enable_remote_keyguard_animation=0
 
+# Disable remote keyguard animation
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.wm.enable_remote_keyguard_animation=0
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce

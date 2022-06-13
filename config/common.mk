Spark-Rom/platform_vendor_spark

vendor: Add QuickTap sysconfig
Change-Id: Ib80bd3edec4b197603c4b9937428db2536d84e0a

config/common.mk
@@ -227,3 +227,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     hosts.spark_adblock
+
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/spark/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif
+

BlissRoms/platform_vendor_bliss

config/common.mk
@@ -198,3 +198,9 @@ endif
 
 # System
 persist.sys.binary_xml=false
+
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif
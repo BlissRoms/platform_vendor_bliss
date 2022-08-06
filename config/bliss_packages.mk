0327f9285c09ddc5c678b95b02dd0c3779ad49ba
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-08-06 15:13:56 -0500

    bliss: Build TouchGestures
    Change-Id: I21f4b8a26b0242652a0c5277a6727d76977c8267

--- config/bliss_packages.mk
+++ config/bliss_packages.mk
@@ -98,3 +98,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+# TouchGestures
+PRODUCT_PACKAGES += \
+    TouchGestures

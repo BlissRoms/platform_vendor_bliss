3604f259dc206194f2ec765cde66759c7546e8a9
Tim Zimmermann <tim@linux4.de>
2022-09-13 08:51:55 +0200

    bliss: Support charger on vendor
    Change-Id: I7cfd4cdaf4e3db48e3a970cb103b35986ec290cf

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -12,7 +12,8 @@ PRODUCT_PACKAGES += \
 
 ifneq ($(WITH_LINEAGE_CHARGER),false)
 PRODUCT_PACKAGES += \
-    lineage_charger_animation
+    lineage_charger_animation \
+    lineage_charger_animation_vendor
 endif
 
 # Customizations

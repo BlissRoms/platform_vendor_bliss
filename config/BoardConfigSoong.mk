fc326dd3ed52ee6a28df2e99b17306d76203d024
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-18 00:31:32 -0400

    soong: Bring bootloader_message_offset back
    This is squashed revert of:
    
        Revert "config: Remove BOOTLOADER_MESSAGE_OFFSET"
    
        This reverts commit a8bbd0e4aa41915818774984e04c977afe234498.
    
        Change-Id: I66df34e901e87e543102377469a150a9bf03e5b8
    
        Revert "Revert "soong: Export bootloader_message_offset to dependencies""
    
        This reverts commit d0b4f9894add43e5caa8720575391e781cab2cf2.
    
        Reason for revert: Bringing this back, LGE 8996 lives.
    
        Change-Id: Iae68fee53ba647d8c64c54388e7296232adf4db8
    
    Change-Id: I358d8e19f2d4da590c9579c1473b5f1964bb71aa
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += blissGlobalVars
 SOONG_CONFIG_blissGlobalVars += \
     aapt_version_code \
     additional_gralloc_10_usage_bits \
+    bootloader_message_offset \
     camera_needs_client_info \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
@@ -78,6 +79,7 @@ SOONG_CONFIG_blissGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blissGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
+BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -86,6 +88,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_blissGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
+SOONG_CONFIG_blissGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_blissGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_blissGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_blissGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))

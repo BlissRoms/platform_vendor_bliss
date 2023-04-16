63fb0c580446877b796dc395b73f1fc28fb49d61
Jackeagle <jackeagle102@gmail.com>
2023-04-16 13:12:33 -0400

    bliss: Switch to MindGapps
    Change-Id: I4f110375f2942ec732d2c67b6a2b90a74f03db6e
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -184,7 +184,7 @@ PRODUCT_COPY_FILES += \
 
 # Gapps
 ifeq ($(BLISS_BUILD_VARIANT), gapps)
-$(call inherit-product, vendor/gms/products/gms.mk)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
 # Plugins

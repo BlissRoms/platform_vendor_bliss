1de1c911df0f48897c8155c59d5b91d15ae63921
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2023-06-25 17:10:37 -0500

    vendor: soong: Add camera_needs_client_info_lib
    Change-Id: Iee325eb37c470178feb03674dc29e27c09a59faa

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,6 +37,7 @@ SOONG_CONFIG_blissGlobalVars += \
     target_surfaceflinger_udfps_lib \
     uses_egl_display_array \
     gralloc_handle_has_reserved_size \
+    camera_needs_client_info_lib \
     needs_camera_boottime \
     uses_oplus_camera \
     uses_nothing_camera
@@ -69,6 +70,7 @@ SOONG_CONFIG_blissQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_
 SOONG_CONFIG_blissQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blissQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_blissGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_blissGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_blissGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blissGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blissGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)

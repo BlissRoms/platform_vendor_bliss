108fd65ee887efdeb3d9fcd166f1408703224323
srgrusso <srgrusso240@gmail.com>
2023-06-25 14:54:02 -0500

    vendor: soong: Fix camera_needs_client_info_lib
    Change-Id: I575caedd8fe33a7014444553d406270a735a3682

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -50,6 +50,7 @@ SOONG_CONFIG_blissQcomVars += \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd \
     uses_qti_camera_device \
+    camera_needs_client_info_lib \
     needs_camera_boottime_timestamp
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -68,6 +69,7 @@ SOONG_CONFIG_blissQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_
 SOONG_CONFIG_blissQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_blissQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_blissGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_blissGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_blissQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blissGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blissGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)

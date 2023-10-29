a0974cf4775d6dfde9d679084783dd68ccd27bfb
Nolen Johnson <johnsonnolen@gmail.com>
2023-10-29 15:18:32 -0400

    config: telephony: Advertise telephony support for GMS
    Change-Id: Ic5063b0d5e60f8bc3846cc8a25c24e1abfa9df84

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,3 +1,8 @@
+# GMS
+ifeq ($(WITH_GMS),true)
+WITH_GMS_COMMS_SUITE := true
+endif
+
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml

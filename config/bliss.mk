512c2625128a7adcaaf90f4c7504f4252cf3324a
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    bliss: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -12,6 +12,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys" \
     PihooksGmsModel="Pixel 9"
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

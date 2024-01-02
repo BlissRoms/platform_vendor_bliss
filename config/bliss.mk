1e8521b5e563ad00369dffaf7a0da50d13afcfeb
John Galt <johngaltfirstrun@gmail.com>
2024-01-02 11:36:34 -0500

    bliss: Add preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Change-Id: Id6a451d6afd91d560de449fc1d3d57fadceb509d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -1,3 +1,7 @@
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

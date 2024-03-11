b13e7be87db330c98dfbdb1c0f10508ffc7b55c8
cjh1249131356 <cjh1249131356@gmail.com>
2024-03-10 22:18:41 -0500

    soong: Add support to set vendor tag with client package name
    * OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    
    Change-Id: I5e56a66900c389d9b31aaabe4eef1d84ff2b59bf

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -53,7 +53,11 @@ SOONG_CONFIG_blissGlobalVars += \
     target_health_charging_control_supports_toggle \
     camera_needs_client_info_lib \
     camera_needs_client_info_lib_oplus \
-    needs_camera_boottime
+    needs_camera_boottime \
+    uses_nothing_camera \
+    uses_oplus_camera \
+    uses_oppo_camera
+
 
 SOONG_CONFIG_NAMESPACES += blissNvidiaVars
 SOONG_CONFIG_blissNvidiaVars += \
@@ -93,6 +97,9 @@ SOONG_CONFIG_blissGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGE
 SOONG_CONFIG_blissGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_blissGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_blissGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_blissGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
+SOONG_CONFIG_blissGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_blissGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

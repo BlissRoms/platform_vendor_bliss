90439ef490284c0918faa03049677ad6907ac86a
someone5678 <someone5678@users.noreply.github.com>
2024-07-17 14:11:04 -0400

    bliss: setup TARGET_RELEASE nd aconfig flags
    Change-Id: I72045560a3dfaa687e4400e1e234c1f6b5539992
    Co-authored-by: Daniel <danielml3@users.noreply.github.com>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

---
+++ release/release_config_map.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2024 TheParasiteProject
+# Copyright (C) 2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_RELEASE := ap2a
+
+# Get the directory for this file, and use that instead of a fixed path.
+local_dir := $(dir $(lastword $(MAKEFILE_LIST)))
+
+# Attach the flag value definitions to the various release configurations.
+$(call declare-release-config, $(TARGET_RELEASE), $(local_dir)build_config/$(TARGET_RELEASE).scl)
+
+local_dir :=

e9655e406d7dd28c8f7746b9f1dee19392882c78
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 22:01:35 +0200

    config: tablet: Enable freeform and large screen support for all targets
    Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,8 +1,8 @@
 # Inherit mobile full common BlissRoms stuff
 $(call inherit-product, vendor/bliss/config/common_mobile_full.mk)
 
-# Inherit full tablet common BlissRoms stuff
-$(call inherit-product, vendor/bliss/config/full_tablet.mk)
+# Inherit tablet common BlissRoms stuff
+$(call inherit-product, vendor/bliss/config/tablet.mk)
 
 $(call inherit-product, vendor/bliss/config/telephony.mk)
 
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -3,4 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 # Inherit mobile mini common Bliss stuff
 $(call inherit-product, vendor/bliss/config/common_mobile_mini.mk)
 
+# Inherit tablet common Bliss stuff
+$(call inherit-product, vendor/bliss/config/tablet.mk)
+
 $(call inherit-product, vendor/bliss/config/wifionly.mk)

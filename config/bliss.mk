df0bb8cd85be731bc37ce98400918c8faa899dd1
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 07:58:33 +0800

    bliss: Disable memtag for different processes
    * it defaults to async if not specified
    
    Change-Id: I74450d822b6ddae6727372de82ff274d0e73ac8a
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -2,6 +2,13 @@
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-bliss-product.xml
 
+# Disable async MTE on a few processes
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

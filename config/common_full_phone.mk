LineageOS/android_vendor_lineage

config: Rename mobile-specific configs
Since common_full.mk and common_mini.mk are mobile-specific, rename them

Change-Id: I2c47fd0688350ac815ccecbff7b453d7fab5d5ea

config/common_full_phone.mk
@@ -1,5 +1,5 @@
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+# Inherit mobile full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+# Inherit mobile full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+# Inherit mobile full common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_mini_tablet.mk
@@ -1,5 +1,5 @@
-# Inherit mini common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mini.mk)
+# Inherit mobile mini common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_mobile_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

BlissRoms/platform_vendor_bliss

Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

config/common_full_phone.mk
@@ -1,5 +1,5 @@
-# Inherit full common Bliss stuff
-$(call inherit-product, vendor/bliss/config/common_full.mk)
+# Inherit mobile full common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit full common BlissRoms stuff
-$(call inherit-product, vendor/bliss/config/common_full.mk)
+# Inherit mobile full common BlissRoms stuff
+$(call inherit-product, vendor/bliss/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit full common Bliss stuff
-$(call inherit-product, vendor/bliss/config/common_full.mk)
+# Inherit mobile full common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_mobile_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \

config/common_mini_tablet.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit mini common BlissRoms stuff
-$(call inherit-product, vendor/bliss/config/common_mini.mk)
+# Inherit mobile mini common BlissRoms stuff
+$(call inherit-product, vendor/bliss/config/common_mobile_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
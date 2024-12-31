6ea1f06ddee386add8d11e6e2b13c5582708286e
HeCheng Yu <kde-yyds@qq.com>
2024-12-31 23:54:44 +0900

    bliss: Disable default frame rate limit for games
    * Android 15 limits refresh rate to 60hz for games unless we enable
      the developer option 'Disable default frame rate for games' manually.
    * Adding this prop enables it by default.
    
    Change-Id: I8981643d7901726434e0add9a21a9131e50737a8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -9,6 +9,10 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.arm64.memtag.app.com.android.nfc=off \
     persist.arm64.memtag.process.system_server=off
 
+# Disable default frame rate limit for games
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=true
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

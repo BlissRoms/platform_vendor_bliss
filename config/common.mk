3ef61a3bd733efc61bf66d0c9cbee69bbb2e0fd3
hmtheboy154 <buingoc67@gmail.com>
2025-01-09 14:43:58 +0700

    bliss: Handle insecure ADB like on Lineage
    This commit basically do 2 things:
    - Revert "bliss: Allow ADB by default except for user builds"
    https://github.com/BlissRoms/platform_vendor_bliss/commit/0686ef9cfa78fba2cdfd0fb1220cf0160897eac7
    - Apply "lineage: config: Allow using an env var to enable insecure ADB" from LOS
    https://github.com/LineageOS/android_vendor_lineage/commit/2eb893f71608c4dbb45e8feee2247e8012f2f486
    
    ADB secure should be on by default on userdebug builds.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -13,12 +13,17 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+else
+ifdef WITH_ADB_INSECURE
+# Forcebly disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 else
 # Enable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
 endif
 
 # Enable one-handed mode

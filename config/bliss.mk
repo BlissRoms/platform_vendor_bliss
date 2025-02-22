f4cd3b9e3130229370b01eb5b89c52c4308e3e4a
Joey Huab <jhuab@edukasyon.ph>
2025-02-22 21:53:08 +0900

    config: properties: March 2025 pihooks update
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -2,15 +2,15 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=tokay_beta \
-    persist.sys.pihooks_DEVICE?=tokay \
-    persist.sys.pihooks_ID?=BP22.250124.009 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-02-05 \
+    persist.sys.pihooks_PRODUCT?=oriole_beta \
+    persist.sys.pihooks_DEVICE?=oriole \
+    persist.sys.pihooks_ID?=BP22.250221.013 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-03-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys" \
-    PihooksGmsModel="Pixel 9"
+    PihooksGmsFp="google/oriole_beta/oriole:16/BP22.250221.013/13218340:user/release-keys" \
+    PihooksGmsModel="Pixel 6"
 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \

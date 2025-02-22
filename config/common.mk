9f24a7a8b6a27c8d8199ab876f7eca478434aaec
LuK1337 <priv.luk@gmail.com>
2025-02-22 17:04:11 +0100

    vendor: config: Install FRP wipe script into recovery /system/bin
    Change-Id: I24e217e6af87f2002193ac7b6defb158cce0a776
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- config/common.mk
+++ config/common.mk
@@ -129,6 +129,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/%/libfuse-lite.so \
     system/%/libntfs-3g.so
 
+# FRP
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true

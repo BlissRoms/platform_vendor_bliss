e19879d1b21f2163f6125d57ba0ca21c83201049
Dhina17 <dhinalogu@gmail.com>
2025-02-24 21:09:45 -0600

    soong: Add libcameraservice extension config
    Change-Id: I4210f5e4845bed58b608fb798597c9a9cbccc832

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -60,6 +60,7 @@ SOONG_CONFIG_blissGlobalVars += \
     uses_nothing_camera \
     uses_oplus_camera \
     uses_oppo_camera \
+    target_libcameraservice_ext_lib \
     target_power_libperfmgr_mode_extension_lib \
     spoof_first_api_level_32 \
     target_powershare_path \
@@ -119,6 +120,7 @@ SOONG_CONFIG_blissGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
+TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
@@ -155,6 +157,7 @@ SOONG_CONFIG_blissGlobalVars_target_health_charging_control_supports_deadline :=
 SOONG_CONFIG_blissGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
 SOONG_CONFIG_blissGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_blissGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_blissGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 SOONG_CONFIG_blissGlobalVars_target_power_libperfmgr_mode_extension_lib := $(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB)
 SOONG_CONFIG_blissGlobalVars_target_powershare_path := $(TARGET_POWERSHARE_PATH)
 SOONG_CONFIG_blissGlobalVars_target_powershare_enabled := $(TARGET_POWERSHARE_ENABLED)

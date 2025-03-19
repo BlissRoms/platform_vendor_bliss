b3643bc76626fca29c0cf5831c0ddfee8d743e04
minaripenguin <minaripenguin@users.noreply.github.com>
2025-03-19 05:29:06 -0400

    config: properties: Add pihooks default values
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -1,3 +1,17 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=tokay_beta \
+    persist.sys.pihooks_DEVICE?=tokay \
+    persist.sys.pihooks_ID?=BP22.250124.009 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-02-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/tokay_beta/tokay:Baklava/BP22.250124.009/13034193:user/release-keys" \
+    PihooksGmsModel="Pixel 9"
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

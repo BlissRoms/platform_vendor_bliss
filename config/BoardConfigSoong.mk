f506bc85b7ff2fa33e853fde1a233c2450ba81fe
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 11:40:16 +0200

    soong: drop librmnetctl config
    Flag hasn't been ported since lineage-19.1.
    
    Change-Id: I2078d19cc80e4b64b86e2aab0bd68d7ee0f14224
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -68,9 +68,6 @@ SOONG_CONFIG_lineageNvidiaVars += \
     uses_nvidia_enhancements
 
 SOONG_CONFIG_NAMESPACES += lineageQcomVars
-SOONG_CONFIG_lineageQcomVars += \
-    uses_pre_uplink_features_netmgrd
-
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_lineageQcomVars += \
@@ -88,7 +85,6 @@ SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPL
 SOONG_CONFIG_lineageGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_lineageGlobalVars_spoof_first_api_level_32 := $(SPOOF_FIRST_API_LEVEL_32)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_lineageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

7b357d6b8517a360a7b0a981bad381c5cf9fb78e
Jackeagle <jackeagle102@gmail.com>
2025-04-06 14:41:59 -0400

    config: Build our Updater instead of LOS Updater
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -136,7 +136,7 @@ endif
 
 PRODUCT_PACKAGES += \
     LineageSettingsProvider \
-    Updater
+    BlissUpdater
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc

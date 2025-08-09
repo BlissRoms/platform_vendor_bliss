1969511ac1b27ba74d7de767790b3c479d427f65
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:08:33 +0200

    Reapply "bliss: Add framework compatibility matrix for Lineage HALs"
    It's time to fix your devices.
    
    This reverts commit 9887e1f75199cb37af46f13b53b3b5a8464d8466.
    
    Change-Id: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -270,6 +270,8 @@ CUSTOM_LOCALES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/bliss/config/device_framework_matrix.xml
+
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/bliss/build/target/product/security/lineage
 

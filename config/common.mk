c1a6bc23da87efe1aa3f6cb2c74dd6311465ce7b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 01:43:51 +0800

    lineage: Move build-manifest.xml build rule into `build/tasks`
    * For Android.mk deprecation
    
    Change-Id: I4aba949ae0af7674955d7a2c439d191348b41f3c

--- config/common.mk
+++ config/common.mk
@@ -124,10 +124,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(BLISS_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# Build Manifest
-PRODUCT_PACKAGES += \
-    build-manifest.xml
-
 # Lineage packages
 ifeq ($(PRODUCT_IS_ATV),)
 PRODUCT_PACKAGES += \

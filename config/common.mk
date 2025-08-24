1ad24b6d992938e270becc965490570a773c644b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 01:43:51 +0800

    bliss: Move build-manifest.xml build rule into `build/tasks`
    * For Android.mk deprecation
    
    Change-Id: I4aba949ae0af7674955d7a2c439d191348b41f3c
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -123,10 +123,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# Build Manifest
-PRODUCT_PACKAGES += \
-    build-manifest.xml
-
 # Lineage packages
 ifeq ($(PRODUCT_IS_ATV),)
 PRODUCT_PACKAGES += \

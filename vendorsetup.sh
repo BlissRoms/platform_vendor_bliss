LineageOS/android_vendor_lineage

lineage: Convert build-manifest.xml to Android.bp
Change-Id: I2c629fc33af93232050834ac347674e7bb7cd8be

vendorsetup.sh
@@ -1,0 +1,4 @@
+#!/bin/bash -e
+
+echo "- Generating vendor/lineage/prebuilt/generated/build-manifest.xml"
+python3 .repo/repo/repo manifest -o - -r | grep -Ev "proprietary_${MANIFEST_EXCLUDES}" > vendor/lineage/prebuilt/generated/build-manifest.xml

BlissRoms/platform_vendor_bliss

Signed-off-by: Jackeagle <jackeagle102@gmail.com>

vendorsetup.sh
@@ -1,0 +1,4 @@
+#!/bin/bash -e
+
+echo "- Generating vendor/bliss/prebuilt/generated/build-manifest.xml"
+python3 .repo/repo/repo manifest -o - -r | grep -Ev "proprietary_${MANIFEST_EXCLUDES}" > vendor/bliss/prebuilt/generated/build-manifest.xml
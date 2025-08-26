0ab1ae43958ff95f5f9bad97d77d43900eef772a
minaripenguin <minaripenguin@users.noreply.github.com>
2025-08-26 00:15:40 -0400

    config: properties: Add pihooks default values
    Change-Id: I9d6af6797ef44673e31db2e7a0ea58ca2c735102
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -38,3 +38,19 @@ include vendor/bliss-priv/keys/keys.mk
 else
 -include vendor/bliss-priv/keys/keys.mk
 endif
+
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP41.250725.006 \
+    persist.sys.pihooks_RELEASE?=12 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-08-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/cheetah_beta/cheetah:Baklava/BP41.250725.006/13939570:user/release-keys" \
+    PihooksGmsModel="Pixel 7 Pro"

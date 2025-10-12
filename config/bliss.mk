d203b722b4f4f4886909cab545c0f6aad4f053ae
Joey Huab <joey@evolution-x.org>
2025-10-12 01:01:41 -0400

    vendor: Update piHooks default values
    * A16 QPR2 Beta 2.
    
    Change-Id: I78248cccbd0ad6b233381f13b5202391e1089d94
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -43,14 +43,14 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=cheetah_beta \
-    persist.sys.pihooks_DEVICE?=cheetah \
-    persist.sys.pihooks_ID?=BP41.250725.006 \
+    persist.sys.pihooks_PRODUCT?=husky_beta \
+    persist.sys.pihooks_DEVICE?=husky \
+    persist.sys.pihooks_ID?=BP41.250822.010 \
     persist.sys.pihooks_RELEASE?=12 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-08-05 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-09-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/cheetah_beta/cheetah:Baklava/BP41.250725.006/13939570:user/release-keys" \
-    PihooksGmsModel="Pixel 7 Pro"
+    PihooksGmsFp="google/husky_beta/husky:16/BP41.250822.010/14082742:user/release-keys" \
+    PihooksGmsModel="Pixel 8 Pro"

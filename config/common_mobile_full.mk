1211ff5c991ea8cd31a32fc12cccb1a2a3544226
Jis G Jacob <studiokeys@blissroms.org>
2025-12-14 07:30:53 -0500

    vendor: Changes to incorporate fonts
    Change-Id: I039b1ebe9bbc3e86185144b8056fd99376ffef6d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -32,13 +32,6 @@ PRODUCT_PACKAGES += \
     unrar \
     zstd
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontGoogleSansFlexOverlay \
-    FontLatoOverlay \
-    FontRubikOverlay
-
 # Include Bliss LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/bliss/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/bliss/overlay/dictionaries

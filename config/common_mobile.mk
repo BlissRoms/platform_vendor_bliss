7ffc0bcf987367dc4850052eb109c8ae4749677f
Michael Bestas <mkbestas@lineageos.org>
2025-12-30 02:20:39 +0200

    config: Stop building icon packs & shapes
    BP3A refactored ThemePicker so our additions are no longer visible in
    the UI. Additionally, the icon packs barely apply to icons since the
    compose rewrite in SystemUI. BP4A has added icon shape selection but
    implemented differently than what we had previously.
    
    Users that upgrade from 23.0 and had custom icons set won't be able to
    unset them, so stop building them.
    
    In the future, we could bring the missing icon shapes back but
    reimplemented to show in the new picker UI.
    
    Change-Id: If41c5990a67340c04ca251f267fdc8e2732bc864
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -47,40 +47,6 @@ PRODUCT_PACKAGES += \
     lineage_charger_animation_vendor
 endif
 
-# Customizations
-PRODUCT_PACKAGES += \
-    IconPackCircularAndroidOverlay \
-    IconPackCircularLauncherOverlay \
-    IconPackCircularSettingsOverlay \
-    IconPackCircularSystemUIOverlay \
-    IconPackFilledAndroidOverlay \
-    IconPackFilledLauncherOverlay \
-    IconPackFilledSettingsOverlay \
-    IconPackFilledSystemUIOverlay \
-    IconPackKaiAndroidOverlay \
-    IconPackKaiLauncherOverlay \
-    IconPackKaiSettingsOverlay \
-    IconPackKaiSystemUIOverlay \
-    IconPackRoundedAndroidOverlay \
-    IconPackRoundedLauncherOverlay \
-    IconPackRoundedSettingsOverlay \
-    IconPackRoundedSystemUIOverlay \
-    IconPackSamAndroidOverlay \
-    IconPackSamLauncherOverlay \
-    IconPackSamSettingsOverlay \
-    IconPackSamSystemUIOverlay \
-    IconPackVictorAndroidOverlay \
-    IconPackVictorLauncherOverlay \
-    IconPackVictorSettingsOverlay \
-    IconPackVictorSystemUIOverlay \
-    IconShapePebbleOverlay \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTaperedRectOverlay \
-    IconShapeTeardropOverlay \
-    IconShapeVesselOverlay
-
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true

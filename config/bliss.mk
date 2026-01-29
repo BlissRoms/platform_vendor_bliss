947914690c7ba5fdea2417dfb92d72885dbf4586
Pranav Vashi <neobuddy89@gmail.com>
2026-01-29 23:20:14 +0530

    config: Fix material expressive for permission manager
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/887
    Change-Id: If3a41c5d99e10aa09b289146f9f9d5a72ae0c179
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/bliss.mk
+++ config/bliss.mk
@@ -3,6 +3,9 @@ PRODUCT_PACKAGES += \
     BatteryStatsViewer \
     OmniJaws
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += is_expressive_design_enabled=true
+
 # Keys
 ifeq ($(BLISS_BUILD_TYPE),OFFICIAL)
 include vendor/bliss-priv/keys/keys.mk

de4e03068275af0ca2541847c44e5406ed2806d8
Jackeagle <jackeagle102@gmail.com>
2026-02-01 22:43:02 -0500

    Revert "config: tablet: Disable desktop windowing mode"
    - latest patchsets have better commits
    
    This reverts commit 31730c5114a06c8d080768511303872bbdda18b4.

--- config/tablet.mk
+++ config/tablet.mk
@@ -1,4 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/large_screen_common.mk)
 
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
 # Tablet-specific overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/bliss/overlay/tablet

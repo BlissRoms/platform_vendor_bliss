ac2dbd2ab0f5531fc094b5b8725a3f29490c526a
Jackeagle <jackeagle102@gmail.com>
2026-04-10 23:18:12 -0400

    config: Welcome BlissROMs v19.5
    Change-Id: Id6eb57d0961a411f41fe379242a6e3e1125385fc
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -6,7 +6,7 @@
 #   BLISS_BUILD_VARIANT - Build variant (vanilla, gapps, etc.) - defaults to vanilla
 
 BLISS_VERSION_MAJOR := 19
-BLISS_VERSION_MINOR := 4
+BLISS_VERSION_MINOR := 5
 BLISS_CODENAME := Waterlily
 BLISS_PROJECT := BlissRoms
 

d1ec18f67ce76496c642590b1057df9aa6174e1c
Aaron Kling <webgeek1234@gmail.com>
2026-04-18 20:11:06 -0500

    kernel: Support having kernel platform in the main repo manifest
    This requires two changes:
    * Block the directory containing these trees from being scanned by bp
    * Parse the full manifest and trim it down to only the relevant kernel
      platform repos for use by the bazel build system to generate module
      versions
    
    Change-Id: I2cb1d2852a5ce7c8e67ca96df01ab50fb720250c
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,9 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/bliss/config/bliss.mk)
 $(call inherit-product, packages/overlays/Bliss/bliss_packages.mk)
 
+# Exclude kernel platform repos from bp scanning
+PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 

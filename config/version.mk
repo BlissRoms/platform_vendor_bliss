0a978ef70360a0825a76f25d1512a31e27c34713
Jis G Jacob <studiokeys@blissroms.org>
2026-05-21 04:42:16 -0400

    versions: Gear up for Bliss 19.6
    Change-Id: I2c0ddfcfa49cd584bceb7dec2981e0a5d87a68d4
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -6,7 +6,7 @@
 #   BLISS_BUILD_VARIANT - Build variant (vanilla, gapps, etc.) - defaults to vanilla
 
 BLISS_VERSION_MAJOR := 19
-BLISS_VERSION_MINOR := 5
+BLISS_VERSION_MINOR := 6
 BLISS_CODENAME := Waterlily
 BLISS_PROJECT := BlissRoms
 

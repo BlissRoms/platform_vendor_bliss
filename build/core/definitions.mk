d7d6024b751686f32a5677b45922176d61cbcf9e
LuK1337 <priv.luk@gmail.com>
2026-09-23 18:55:31 +0200

    Revert "build: Drop build-manifest.xml generation"
    This reverts commit f31803fcef2a967774548dff24318db35e8ab4f0.
    
    Change-Id: I563c90203f5d9841c6098976d93d9e7117cfda8d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -7,3 +7,6 @@ endef
 
 # Include board/platform macros
 include vendor/bliss/build/core/utils.mk
+
+# Include default installed modules
+include vendor/bliss/build/core/default_installed_modules.mk
